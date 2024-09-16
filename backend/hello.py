36104d9e28697b92f07962b96d1098eda5c7245a
Sadman Sami <112992400+S-a-m-i-0@users.noreply.github.com>
2024-09-15 20:45:13 -0600

    Delete backend/hello.py

--- backend/hello.py
+++
@@ -1,5 +0,0 @@
-from tensorflow.keras.models import load_model
-
-# Load the model from the .keras or .h5 file
-loaded_model = load_model('my_emotion_model.keras')  # or 'my_emotion_model.h5'
-loaded_model.summary()
